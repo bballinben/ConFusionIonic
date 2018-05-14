ea38313a28edb42c93927dd8202f793107dd4735
Benjamin King <benjaminking106@gmail.com>
2018-05-13 18:27:02 -0700

    fab button

--- conFusion/src/pages/about/about.ts
+++ conFusion/src/pages/about/about.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { LeaderProvider } from '../../providers/leader/leader';
 
 /**
  * Generated class for the AboutPage page.
@@ -16,10 +17,18 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 export class AboutPage {
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
+    @Inject('BaseURL') private BaseURL) {
+      this.leader = navParams.get('leader');
+      this.leaderdesc = this.leader.desc.length;
+      let total = 0;
+    }
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad AboutPage');
   }
 
+  ngOnInit(
+    
+  )
 }
